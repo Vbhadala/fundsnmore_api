fcc0b2996380c9638c9a4084936dbeb06d0f343d
Vbhadala <49553287+Vbhadala@users.noreply.github.com>
2023-06-24 20:29:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ from fastapi.encoders import jsonable_encoder
 import yfinance as yf
 from datetime import datetime
 
-
 import datetime
 import requests
 import json
 import pandas as pd
+pd.set_option('mode.chained_assignment', None)
 import mibian
 
 app = FastAPI()
@@ -20,16 +20,23 @@ app = FastAPI()
 def first_api():
         return {'message': 'Hello Vinod'}
 
-BOOKS = [{"title": 'Title 1'},{"title": 'Title 2'},{"title": 'Title 3'}]
 
-from nifty import fetch_nifty, compute_greeks, patch,process_row
+from nifty import fetch_nifty, compute_greeks, patch,process_row, operation,calculate_max_pain
 
 @app.get("/nifty")
 
 def nifty():
-        df = fetch_nifty("29-Jun-2023")
-        df_json = df.to_dict(orient='records')
-        return df_json
+        r = fetch_nifty("29-Jun-2023")
+        
+        df_chain_json = r[0].to_dict(orient='records')
+        df_ops_json = r[1].to_dict(orient='records')
+        
+        return df_chain_json, df_ops_json
+    
+
+        return type(r)
+
+
 
 
 
